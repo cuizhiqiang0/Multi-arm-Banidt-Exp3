b0cc8a64c99046dc91d373b3138b533e2c0558ed
Qingyun Wu <qw2ky@virginia.edu>
2015-01-20 13:46:43 -0500

    Exp3

---
+++ conf.py
@@ -0,0 +1,10 @@
+import os
+
+datasets_address = "/Users/Summer/Documents/Research/datasets/"
+
+yahoo_address = os.path.join(datasets_address, "R6")
+
+# for my personal external drive if i want to run code from dataset stored on my drive.
+yahoo_address_ExternalDrive = "/Volumes/My Passport/GradSchool/MSThesis/datasets/Yahoo/"
+
+save_address = "/Users/Summer/Dropbox/MyResearch/Code/MyCode/Run15_1"
