973a57a5fff07ccd1c64f292181438dda034edfa
Qingyun Wu <qw2ky@virginia.edu>
2015-01-22 21:58:51 -0500

    FinalAnslysis

--- AnalysisExp3Hours.py
+++ AnalysisExp3Hours.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
     filenames = [x for x in os.listdir(save_address) if 'csv' in x]
     for x in filenames:
         filename = os.path.join(save_address, x)
-        if '0.5_Hour'in x:
+        if '0.3_Hour'in x:
             with open(filename, 'r')as f:
+                
                 randa = {}
                 randc = {}
                 exp3a = {}
@@ -26,7 +27,7 @@ if __name__ == '__main__':
                     tim[i] = int(words[1])
                 plt.plot(tim.values(),exp3CTRRatio.values(),label = '0.5Hour')
 
-        if '0.5_MultipleDay'in x:
+        if '0.5_Hour'in x:
             with open(filename, 'r')as f:
                 randa = {}
                 randc = {}
@@ -44,7 +45,8 @@ if __name__ == '__main__':
                     tim[i] = int(words[1])
                 plt.plot(tim.values(),exp3CTRRatio.values(),label = '0.5MultipleDay')
 
-        if '1_MultipleDay'in x:
+  
+        if '0.7_Hour'in x:
             with open(filename, 'r')as f:
                 randa = {}
                 randc = {}
@@ -60,8 +62,28 @@ if __name__ == '__main__':
                         continue
                     randa[i],randc[i],exp3a[i],exp3c[i],exp3CTRRatio[i] = [float(x) for x in words[2].split(';')]
                     tim[i] = int(words[1])
-                plt.plot(tim.values(),exp3CTRRatio.values(),label = '1_MultipleDay')
-                
+                plt.plot(tim.values(),exp3CTRRatio.values(),label = '0.5Single')  
+        
+        
+                '''
+        if '0.5_MultipleDay'in x:
+            with open(filename, 'r')as f:
+                randa = {}
+                randc = {}
+                exp3a = {}
+                exp3c = {}
+                exp3CTRRatio = {}
+                tim = {}
+                i = -1
+                for line in f:
+                    i = i + 1
+                    words = line.split(',')
+                    if words[0].strip()!='data':
+                        continue
+                    randa[i],randc[i],exp3a[i],exp3c[i],exp3CTRRatio[i] = [float(x) for x in words[2].split(';')]
+                    tim[i] = int(words[1])
+                plt.plot(tim.values(),exp3CTRRatio.values(),label = '0.5_MultipleDay') 
+        
         
         if '0.5_Single'in x:
             with open(filename, 'r')as f:
@@ -79,5 +101,6 @@ if __name__ == '__main__':
                         continue
                     randa[i],randc[i],exp3a[i],exp3c[i],exp3CTRRatio[i] = [float(x) for x in words[2].split(';')]
                     tim[i] = int(words[1])
-                plt.plot(tim.values(),exp3CTRRatio.values(),label = '0.5Single')
-                
+                plt.plot(tim.values(),exp3CTRRatio.values(),label = '0.5Single') 
+        '''
+        
